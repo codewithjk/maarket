0de488ecf610301d5ead289f3c71cdef0708b528
jeevan <jeevankumar8943@gmail.com>
2025-07-01 18:44:39 +0530

    Dev : Initial project setup, created api gateway and set ratelimiter,  Auth - service setup , created custom error hadlers

---
+++ packages/error-handler/error-middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { AppError } from ".";
+
+export const errorMiddleware = (err :Error, req : Request, res :Response,) => {
+    if (err instanceof AppError) {
+        console.log(`Error: ${req.method}, ${req.url}, StatusCode: ${err.statusCode}, Message: ${err.message}`);
+        return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+            ...(err.details && { details : err.details}),
+        });
+    }
+    console.error(`🚫🚫🚫 UnHandled Error: ${req.method}, ${req.url}, Message: ${err.message}`);
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+        details: err.message,
+    }); 
+}
